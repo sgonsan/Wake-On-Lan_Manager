b6b6237a380e54a46c4edd2ca60d5c011f83ef76
sgonsan <Losestancos2003@gmail.com>
2024-10-26 14:18:29 +0000

    Refactor compile_and_execute.sh script to copy executable to /usr/bin

--- compile_and_execute.sh
+++ compile_and_execute.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
+# This script compiles the program and copies the executable to /usr/bin
+
 clear
 # Load variables from config
 source "./config"
-
-# echo -e "${COLOR_YELLOW}Generating documentation...${COLOR_RESET}"
-
-# doxygen ../Doxyfile
-# sleep 0.5 # Pause for 1 second to simulate an animation
-
-# echo -e "${COLOR_GREEN}Documentation generated.${COLOR_RESET}"
-
 
 echo -e "${COLOR_YELLOW}Cleaning and compiling...${COLOR_RESET}"
 
@@ -21,6 +15,6 @@ if [ $? -ne 0 ]; then
   echo -e "${COLOR_RED}Compile error.${COLOR_RESET}"
 else
   echo -e "${COLOR_GREEN}Successful build.${COLOR_RESET}"
-  sudo cp ./build/${PROGRAM_NAME} /bin/${PROGRAM_NAME}
-  echo -e "${COLOR_YELLOW}Executable copied to /bin/${PROGRAM_NAME}${COLOR_RESET}"
+  sudo cp ./build/${PROGRAM_NAME} /usr/bin/${PROGRAM_NAME}
+  echo -e "${COLOR_YELLOW}Executable copied to /usr/bin/${PROGRAM_NAME}${COLOR_RESET}"
 fi
